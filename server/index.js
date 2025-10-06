be9591ffe3d33a63a6c60cd23c775f026cffb5b0
RafaelMota02 <dwayceprdc@gmail.com>
2025-10-06 17:00:21 +0100

    Temporarily allow all CORS origins to fix Vercel API connectivity

--- server/index.js
+++ server/index.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
+  origin: true, // Allow all origins for now
   credentials: true
 }));
 
