7f8da84a142c6feae94f044647fa8cbf76c962d5
RafaelMota02 <dwayceprdc@gmail.com>
2025-10-06 13:49:22 +0100

    Update database connection to support Neon Tech DATABASE_URL

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,6 +1,9 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
+const pool = new Pool(process.env.DATABASE_URL ? { 
+  connectionString: process.env.DATABASE_URL, 
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false 
+} : {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
